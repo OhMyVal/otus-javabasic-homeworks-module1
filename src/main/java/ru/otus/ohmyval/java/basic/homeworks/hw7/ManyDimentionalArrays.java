8caae732b2435f5ccb5a3307c179d4d5f5f18f1e
vadorofeeva <makeup-style@mail.ru>
2023-11-06 22:39:45 +0300

    task 2 done

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw7/ManyDimentionalArrays.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw7/ManyDimentionalArrays.java
@@ -5,10 +5,11 @@ public class ManyDimentionalArrays {
 
         System.out.println(sumOfPositiveElements(new int[][]{{1,3,-5,6},{-2,-4,6,8,9}}));
         System.out.println();
+        printSquare(6);
 
     }
 
-    public static int sumOfPositiveElements(int[][] arr2D){
+    public static int sumOfPositiveElements(int[][] arr2D) {
         int sum = 0;
         for (int i = 0; i < arr2D.length; i++) {
             for (int j = 0; j < arr2D[i].length; j++) {
@@ -20,5 +21,13 @@ public class ManyDimentionalArrays {
         return sum;
     }
 
-
+    public static void printSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("* ");
+                }
+            System.out.println();
+            }
+        System.out.println();
+        }
 }
