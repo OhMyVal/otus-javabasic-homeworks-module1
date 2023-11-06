a2c637c50147eb5f42c69fa07e4439d10f0481f1
vadorofeeva <makeup-style@mail.ru>
2023-11-07 00:00:29 +0300

    task 5 done

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw7/ManyDimentionalArrays.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw7/ManyDimentionalArrays.java
@@ -3,12 +3,13 @@ package ru.otus.ohmyval.java.basic.homeworks.hw7;
 public class ManyDimentionalArrays {
     public static void main(String[] args) {
 
-        System.out.println(sumOfPositiveElements(new int[][]{{1,3,-5,6},{-2,-4,6,8,9}}));
+        System.out.println(sumOfPositiveElements(new int[][]{{1, 3, -5, 6}, {-2, -4, 6, 8, 9}}));
         System.out.println();
         printSquare(6);
-        clearDiagonalArr(new int[][]{{1,3,3,5}, {1,3,2,4}, {1,1,1,1}, {5,1,4,7}});
-        System.out.println(findMax(new int[][]{{1,3,-5,6},{-2,-4,6,8,9,5}}));
+        clearDiagonalArr(new int[][]{{1, 3, 3, 5}, {1, 3, 2, 4}, {1, 1, 1, 1}, {5, 1, 4, 7}});
+        System.out.println(findMax(new int[][]{{1, 3, -5, 6}, {-2, -4, 6, 8, 9, 5}}));
         System.out.println();
+        System.out.println(verticalSum(new int[][]{{1,4}, {-2, 4, 6, 8,}, {1, 3, 3, 5}, {1,4}, {1, 1}, {5, 1, 4, 7}}));
 
     }
 
@@ -16,11 +17,11 @@ public class ManyDimentionalArrays {
         int sum = 0;
         for (int i = 0; i < arr2D.length; i++) {
             for (int j = 0; j < arr2D[i].length; j++) {
-                if (arr2D[i][j] > 0){
+                if (arr2D[i][j] > 0) {
                     sum += arr2D[i][j];
-                    }
                 }
             }
+        }
         return sum;
     }
 
@@ -28,20 +29,20 @@ public class ManyDimentionalArrays {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 System.out.print("* ");
-                }
-            System.out.println();
             }
+            System.out.println();
+        }
         System.out.println();
     }
 
     public static void clearDiagonalArr(int[][] arr2D) {
         for (int i = 0; i < arr2D.length; i++) {
             for (int j = 0; j < arr2D[i].length; j++) {
-                arr2D[i][arr2D.length-1-i] = 0;
+                arr2D[i][arr2D.length - 1 - i] = 0;
                 System.out.print(arr2D[i][j] + " ");
-                }
-            System.out.println();
             }
+            System.out.println();
+        }
         System.out.println();
     }
 
@@ -57,4 +58,20 @@ public class ManyDimentionalArrays {
         return max;
     }
 
+    public static int verticalSum(int[][] array2D) {
+        for (int i = 0; i < array2D.length; i++) {
+            for (int j = 0; j < array2D[i].length; j++) {
+                if (array2D[i].length < 2) {
+                    return -1;
+                }
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < array2D.length; i++) {
+            for (int j = 1; j < 2; j++) {     // Строчки 71 и 72 можно заменить на sum += array2D[i][1];
+                sum += array2D[i][j];
+                }
+            }
+        return sum;
+    }
 }
