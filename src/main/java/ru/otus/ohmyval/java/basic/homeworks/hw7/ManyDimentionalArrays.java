bbf5ec6eadd115c02d9e4fbeb9d2dc2bdcde7439
vadorofeeva <makeup-style@mail.ru>
2023-11-06 23:09:58 +0300

    task 3 done

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw7/ManyDimentionalArrays.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw7/ManyDimentionalArrays.java
@@ -6,6 +6,7 @@ public class ManyDimentionalArrays {
         System.out.println(sumOfPositiveElements(new int[][]{{1,3,-5,6},{-2,-4,6,8,9}}));
         System.out.println();
         printSquare(6);
+        clearDiagonalArr(new int[][]{{1,3,3,5}, {1,3,2,4}, {1,1,1,1}, {5,1,4,7}});
 
     }
 
@@ -29,5 +30,19 @@ public class ManyDimentionalArrays {
             System.out.println();
             }
         System.out.println();
-        }
+    }
+
+    public static void clearDiagonalArr(int[][] arr2D) {
+        for (int i = 0; i < arr2D.length; i++) {
+            for (int j = 0; j < arr2D[i].length; j++) {
+                arr2D[i][arr2D.length-1-i] = 0;
+                System.out.print(arr2D[i][j] + " ");
+                }
+            System.out.println();
+            }
+        System.out.println();
+    }
+
+
+
 }
