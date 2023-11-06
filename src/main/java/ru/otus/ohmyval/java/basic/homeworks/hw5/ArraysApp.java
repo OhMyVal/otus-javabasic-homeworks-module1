1404d9cc9bf903bb853fd65ed0329ede65a2198c
vadorofeeva <makeup-style@mail.ru>
2023-11-06 17:50:01 +0300

    task 1 done

---
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw5/ArraysApp.java
@@ -0,0 +1,16 @@
+package ru.otus.ohmyval.java.basic.homeworks.hw5;
+
+public class ArraysApp {
+    public static void main(String[] args) {
+        printString(6, "Learning to print strings");
+
+    }
+
+    public static void printString (int a, String arg){
+        for (int i = 1; i <= a; i++) {
+            System.out.println(arg);
+        }
+
+        }
+    }
+
