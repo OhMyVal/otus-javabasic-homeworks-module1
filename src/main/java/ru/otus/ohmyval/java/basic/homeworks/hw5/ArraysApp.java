c8848638fecfb3801a1b71d793aab91fb55b8790
vadorofeeva <makeup-style@mail.ru>
2023-11-06 18:19:31 +0300

    task 3 done

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw5/ArraysApp.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw5/ArraysApp.java
@@ -1,15 +1,19 @@
 package ru.otus.ohmyval.java.basic.homeworks.hw5;
 
+import java.util.Arrays;
+
 public class ArraysApp {
     public static void main(String[] args) {
         printString(6, "Learning to print strings");
         sumAndPrint(new int[] {2,4,7,2,9,8,4,6,1,3});
+        fillArr(6, new int[] {3,6,7,8,2,5,9,23,78,93});
     }
 
     public static void printString(int a, String arg) {
         for (int i = 1; i <= a; i++) {
             System.out.println(arg);
         }
+        System.out.println();
     }
 
     public static void sumAndPrint(int [] arr) {
@@ -20,6 +24,16 @@ public class ArraysApp {
             }
         }
         System.out.println(sum);
+        System.out.println();
+    }
+
+    public static void fillArr(int a, int [] arr) {
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = a;
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
     }
 }
 
