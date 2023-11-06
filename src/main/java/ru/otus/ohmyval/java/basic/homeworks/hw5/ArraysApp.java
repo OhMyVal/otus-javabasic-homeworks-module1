6f4474e99e028627b3b5bc74da5e757fd7adf142
vadorofeeva <makeup-style@mail.ru>
2023-11-06 18:29:13 +0300

    task 4 done

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw5/ArraysApp.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw5/ArraysApp.java
@@ -7,6 +7,7 @@ public class ArraysApp {
         printString(6, "Learning to print strings");
         sumAndPrint(new int[] {2,4,7,2,9,8,4,6,1,3});
         fillArr(6, new int[] {3,6,7,8,2,5,9,23,78,93});
+        increaseArr(2, new int[] {6,4,7,2,8,9,1,0,5,44,66});
     }
 
     public static void printString(int a, String arg) {
@@ -35,5 +36,14 @@ public class ArraysApp {
         System.out.println(Arrays.toString(arr));
         System.out.println();
     }
+    public static void increaseArr(int a, int [] arr) {
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] += a;
+            }
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+    }
 }
+
 
