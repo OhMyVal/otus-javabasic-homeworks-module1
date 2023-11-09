b004155b06fdeffe8887c9cc9bbe5a8616b317bc
vadorofeeva <makeup-style@mail.ru>
2023-11-09 23:26:29 +0300

    spiralFill done

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw7/ManyDimentionalArrays.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw7/ManyDimentionalArrays.java
@@ -10,7 +10,9 @@ public class ManyDimentionalArrays {
         System.out.println(findMax(new int[][]{{1, 3, -5, 6}, {-2, -4, 6, 8, 9, 5}}));
         System.out.println();
         System.out.println(verticalSum(new int[][]{{1, 2}, {-2, 5}, {1,6}, {1, 4}, {1, 1}, {5, 1, 4, 7}}));
+        System.out.println();
 
+        spiralFillArray(new int[][]{{1, 2, 3, 4}, {5, 6, 7}, {8, 9, 8, 7, 6, 5, 4}, {3}});
     }
 
     public static int sumOfPositiveElements(int[][] arr2D) {
@@ -70,4 +72,62 @@ public class ManyDimentionalArrays {
         }
         return sum;
     }
+
+    public static void spiralFillArray(int[][] incomingArr) {
+        int quantity = 0;
+        for (int i = 0; i < incomingArr.length; i++) {
+            for (int j = 0; j < incomingArr[i].length; j++) {
+                quantity++;
+            }
+        }
+        int size = (int) (Math.ceil(Math.sqrt(quantity)));
+        int[] interimArr = new int[quantity];
+        int firstCounter = 0;
+        for (int i = 0; i < incomingArr.length; i++) {
+            for (int j = 0; j < incomingArr[i].length; j++) {
+                interimArr[firstCounter] = incomingArr[i][j];
+                firstCounter++;
+            }
+        }
+        int[][] squareArr = new int[size][size];
+        int counter = 0;
+        for (int k = 0; k <= squareArr.length / 2; k++) {
+            for (int j = k; j < squareArr.length - k; j++) {
+                if (counter == interimArr.length) {
+                    break;
+                }
+                squareArr[k][j] = interimArr[counter];
+                counter++;
+            }
+            for (int i = k + 1; i < squareArr.length - k; i++) {
+                if (counter == interimArr.length) {
+                    break;
+                }
+                squareArr[i][squareArr.length - 1 - k] = interimArr[counter];
+                counter++;
+            }
+            for (int j = squareArr.length - 1 - k - 1; j >= k; j--) {
+                if (counter == interimArr.length) {
+                    break;
+                }
+                squareArr[squareArr.length - 1 - k][j] = interimArr[counter];
+                counter++;
+            }
+            for (int i = squareArr.length - 1 - k - 1; i > k; i--) {
+                if (counter == interimArr.length) {
+                    break;
+                }
+                squareArr[i][k] = interimArr[counter];
+                counter++;
+            }
+        }
+        for (int i = 0; i < squareArr.length; i++) {
+            for (int j = 0; j < squareArr.length; j++) {
+                System.out.print(squareArr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
+
