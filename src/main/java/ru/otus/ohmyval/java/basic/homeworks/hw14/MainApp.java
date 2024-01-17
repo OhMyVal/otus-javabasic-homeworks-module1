52bd9ef83815b63eef03a2fe4185fda1471a38c7
vadorofeeva <makeup-style@mail.ru>
2024-01-17 20:17:36 +0300

    Main method created

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw14/MainApp.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw14/MainApp.java
@@ -2,6 +2,17 @@ package ru.otus.ohmyval.java.basic.homeworks.hw14;
 
 public class MainApp {
     public static void main(String[] args) {
+        String[][] arr = {
+                {"10", "2", "4","d"},
+                {"11", "4", "8", "0"},
+                {"5", "6", "3", "7"},
+                {"8", "6", "4", "9"},
+        };
+        try{
+            sumOfArray(arr);
+        }catch (AppArraySizeException | AppArrayDataException ex){
+            System.err.println(ex.getMessage());
+        }
 
     }
     public static void sumOfArray(String[][] arr){
