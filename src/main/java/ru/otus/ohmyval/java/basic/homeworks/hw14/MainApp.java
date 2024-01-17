a2b4020f5751ba2454d7066b484ab01a29e07b11
vadorofeeva <makeup-style@mail.ru>
2024-01-17 20:59:53 +0300

    hw14 done

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw14/MainApp.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw14/MainApp.java
@@ -1,35 +1,37 @@
 package ru.otus.ohmyval.java.basic.homeworks.hw14;
 
+
 public class MainApp {
     public static void main(String[] args) {
         String[][] arr = {
-                {"10", "2", "4","d"},
-                {"11", "4", "8", "0"},
-                {"5", "6", "3", "7"},
-                {"8", "6", "4", "9"},
+                {"10", "2", "4", "5"},
+                {"11", "4", "4", "0"},
+                {"5", "p", "3", "7"},
+                {"8", "6", "4", "3"},
         };
-        try{
-            sumOfArray(arr);
-        }catch (AppArraySizeException | AppArrayDataException ex){
+        try {
+            System.out.println(sumOfArray(arr));
+        } catch (AppArraySizeException | AppArrayDataException ex) {
             System.err.println(ex.getMessage());
         }
 
     }
-    public static void sumOfArray(String[][] arr){
+
+    public static int sumOfArray(String[][] arr) {
         int sum = 0;
-        try {
         for (int i = 0; i < arr.length; i++) {
-            if (arr.length != 4 || arr[i].length != 4){
+            if (arr.length != 4 || arr[i].length != 4) {
                 throw new AppArraySizeException("Некорректный размер массива");
             }
             for (int j = 0; j < arr[i].length; j++) {
-                int intVal = Integer.parseInt(arr[i][j]);
-                sum += intVal;
+                try {
+                    int intVal = Integer.parseInt(arr[i][j]);
+                    sum += intVal;
+                } catch (NumberFormatException ex) {
+                    throw new AppArrayDataException("Ячейку " + arr[i][j] + " невозможно преобразовать в число"); // не знаю, как распечатать индекс массива (и нужно ли это?)
+                }
             }
         }
-        System.out.println(sum);
-        } catch (NumberFormatException ex){
-            throw new AppArrayDataException("Ячейку невозможно преобразовать в число");
-        }
+        return sum;
     }
 }
