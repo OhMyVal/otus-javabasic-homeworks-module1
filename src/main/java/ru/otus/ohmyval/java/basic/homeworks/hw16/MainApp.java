ee9dd70dc6010a02381e4b9b5d84f7b3f0462ffc
vadorofeeva <makeup-style@mail.ru>
2024-03-31 21:23:18 +0300

    fixed after PR comments, method add changed & fixed

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw16/MainApp.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw16/MainApp.java
@@ -1,7 +1,6 @@
 package ru.otus.ohmyval.java.basic.homeworks.hw16;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,11 +12,12 @@ public class MainApp {
 
         phoneBook.add("Ivanov Ivan", "100-400");
         phoneBook.add("Ivanov Ivan", "100-500");
-        phoneBook.add("Ivanov Ivan", "100-500");
+        phoneBook.add("Ivanov Ivan", "100-400");
         phoneBook.add("Petrov Petr", "100-600");
         phoneBook.add("Egorov Egor", "100-700");
         phoneBook.add("Ivanov Sergey", "100-800");
-        phoneBook.add("Petrov Konstantin", "100-900");
+        phoneBook.add("Petrov Konstantin", "100-500");
+        phoneBook.add("Egorov Konstantin", "100-600");
         System.out.println(phoneBookMap.entrySet());
         System.out.println(phoneBookMap.values());
         System.out.println(phoneBookMap.size());
--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw16/PhoneBook.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw16/PhoneBook.java
@@ -1,7 +1,5 @@
 package ru.otus.ohmyval.java.basic.homeworks.hw16;
 
-import jdk.jfr.SettingControl;
-
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
@@ -9,17 +7,15 @@ import java.util.Set;
 
 public class PhoneBook {
     private Map<String, Set<String>> hashMap;
-
+    public PhoneBook(Map<String, Set<String>> hashMap) {
+        this.hashMap = hashMap;
+    }
     public Map<String, Set<String>> getHashMap() {
         return hashMap;
     }
 
-    public PhoneBook(Map<String, Set<String>> hashMap) {
-        this.hashMap = hashMap;
-    }
-
     public void add(String completeName, String number) {
-        if (hashMap.containsValue(Collections.singleton(number))) {
+        if(containsPhoneNumber(number)){
             System.out.println("Номер " + number + " уже есть в телефонном справочнике");
             return;
         }
@@ -30,10 +26,6 @@ public class PhoneBook {
         }
         Set<String> phoneNumbers = new HashSet<>();
         phoneNumbers.addAll(hashMap.get(completeName));
-        if (phoneNumbers.contains(number)) {
-            System.out.println("Номер " + number + " уже есть в телефонном справочнике");
-            return;
-        }
         phoneNumbers.add(number);
         hashMap.put(completeName, phoneNumbers);
         System.out.println("Добавили к " + completeName + " еще один номер " + number);
