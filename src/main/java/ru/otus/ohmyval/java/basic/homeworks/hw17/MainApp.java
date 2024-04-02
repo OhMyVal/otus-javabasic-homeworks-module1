38592784fdc6554e486556ec576ae4940496c0ef
vadorofeeva <makeup-style@mail.ru>
2024-04-02 16:25:18 +0300

    hw17 done

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw17/MainApp.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw17/MainApp.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static ru.otus.ohmyval.java.basic.homeworks.hw17.BubbleSort.bubbleSort;
+
 
 public class MainApp {
     public static void main(String[] args) {
@@ -29,5 +31,8 @@ public class MainApp {
         System.out.println("isManager = " + personDataBase.isManager(new Person("Alex", Position.MANAGER, 12345L)));
         System.out.println("isEmployee = " + personDataBase.isEmployee(34567L));
         System.out.println("isEmployee = " + personDataBase.isEmployee(56789L));
+
+        int[] arr = {7, 6, 5, 4, 3, 8, 9, 9, 9, 9};
+        bubbleSort(arr);
     }
 }
