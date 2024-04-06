2e70862bb762eb24c6ddffe6144ffe3f90a2dd3e
vadorofeeva <makeup-style@mail.ru>
2024-04-07 00:26:30 +0300

    hw26 done

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw26/Box.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw26/Box.java
@@ -1,42 +1,24 @@
 package ru.otus.ohmyval.java.basic.homeworks.hw26;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-import static ru.otus.ohmyval.java.basic.homeworks.hw26.Apple.appleWeight;
 
-
-public class Box <T extends Fruit> {
+public class Box<T extends Fruit> {
     static double emptyBoxWeight = 0.15;
     List<T> list;
 
     public Box(List<T> list) {
         this.list = list;
     }
-    //    public Box() {
-//    }
 
     public static double getEmptyBoxWeight() {
         return emptyBoxWeight;
     }
 
-    public static void setEmptyBoxWeight(double emptyBoxWeight) {
-        Box.emptyBoxWeight = emptyBoxWeight;
+    public List<T> getList() {
+        return list;
     }
-
-//    public List<T> getFruitsInside() {
-//        return fruitsInside;
-//    }
-//
-//    public void setFruitsInside(List<T> fruitsInside) {
-//        this.fruitsInside = fruitsInside;
-//    }
-
-//    @Override
-//    public int compareTo(Object o) {
-//        return 0;
-//    }
 
     @Override
     public boolean equals(Object o) {
@@ -53,33 +35,34 @@ public class Box <T extends Fruit> {
 
     @Override
     public String toString() {
-        return "Box{" +
-                "list=" + list.toString() +
-                '}';
+        return "Box: " + list.toString();
+
     }
 
-   public boolean addFruits(T fruit){
-      return list.add(fruit);
+    public boolean addFruits(T fruit) {
+        return list.add(fruit);
     }
 
-    public double weight(){
+    public double weight() {
         double boxWeight = emptyBoxWeight;
         for (int i = 0; i < list.size(); i++) {
             boxWeight += list.get(i).getFruitWeight();
         }
         return boxWeight;
     }
-    public boolean compare(Box box){
+
+    public boolean compare(Box box) {
         return (Math.abs(weight() - box.weight()) < 0.0001);
     }
-    public boolean transfer(Box <T> box) {
-        if (this.equals(box)){
+
+    public boolean transfer(Box<T> box) {
+        if (this.equals(box)) {
             return false;
         }
         if (list.size() == 0) {
             return false;
         }
-        for (int i = list.size()-1; i >=0; i--) {
+        for (int i = list.size() - 1; i >= 0; i--) {
             box.addFruits(list.get(i));
             list.remove(i);
         }
