fd465cc36c374adfc909c74cfac4024e9e94927c
vadorofeeva <makeup-style@mail.ru>
2024-04-11 17:23:41 +0300

    fish copied

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw20/ExampleClient.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw20/ExampleClient.java
@@ -1,4 +1,27 @@
 package ru.otus.ohmyval.java.basic.homeworks.hw20;
 
-public class ExampleClient {
+import java.io.*;
+
+public class ExampleClient implements AutoCloseable {
+    private final DataInputStream inputStream; // 1. инициализировали переменные
+    private final DataOutputStream outputStream;
+
+    public ExampleClient(InputStream inputStream, OutputStream outputStream) { //2.конструктор создали, в него приходят абстрактные потоки
+        this.inputStream = new DataInputStream(inputStream); // 3. создали потоки ввода/вывода
+        this.outputStream = new DataOutputStream(outputStream);
+    }
+
+    public void send(String message) throws IOException { // метод будет принимать строчку
+        outputStream.writeUTF(message); // отправляет строку на сервер, ждем ответа
+        outputStream.flush(); // сливаем данные
+        String result = inputStream.readUTF(); // ответ получаем из inputStream, получаем строчку-ответ; здесь ждем ответ от сервера - блокирующая операция
+        System.out.println(result); // печатаем результат от сервера
+    }
+
+    @Override
+    public void close() throws Exception { // после завершения работы клиентское приложение закрываем и вызывается метод close
+        inputStream.close(); // закрываем потоки
+        outputStream.close();
+    }
+
 }
--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw20/Server.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw20/Server.java
@@ -1,7 +1,31 @@
 package ru.otus.ohmyval.java.basic.homeworks.hw20;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
-    public static void main(String[] args) {
+    private static final List<ClientHandler> clientHandlers = new ArrayList<>();
+    public static void main(String[] args)  throws IOException {
+        ServerSocket socket = new ServerSocket(8080); // 1.создали сокет, указали порт, к кот. он будет привязываться
+        while(true) { // 2. работает в бесконечном цикле, пока не остановим сами - будет слушать.
+            Socket clientSocket = socket.accept(); // 3. слушает с пом-ю блокирующего метода accept (бесконечно ждет подключения). Когда кто-то подключается, метод accept завершается, выдается инфо о клиенте
+            DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());// 4.сервер получает данные из InputStream (а клиент их на сервер отправляет)
+            DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());// 5.отправляет ответ в клиентское приложение
+            ClientHandler clientHandler = new ClientHandler(clientSocket, inputStream, outputStream); // 13. создали клиентхэндлера
+            clientHandlers.add(clientHandler); // 15. добавили в лист нашего clientHandler
+
+            String userInput = inputStream.readUTF();//7. считали стррчку, кот. пришла от пользователя
+            System.out.println(userInput);// 8. распечатали ее
+            String result = transform(userInput); //9. передали в метод transform
+            outputStream.writeUTF(result); // 10. отдаем обратно пользователю - запихнем в метод writeUTF()
+            outputStream.flush(); // 11. сливаем данные пользователю что бы ни случилось
+            System.out.println(result); // 12 распечатали ее
+        }
 
     }
 }
