ff3e7b01b5270969e2c35a2c486527388f656382
vadorofeeva <makeup-style@mail.ru>
2024-04-14 17:15:22 +0300

    hw20 done

--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw20/Client.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw20/Client.java
@@ -7,32 +7,19 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 8080)) {
-            ExampleClient client = new ExampleClient(socket.getInputStream(), socket.getOutputStream());
-
-//            client.readOperation();
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите операцию из перечисленных: " + client.readOperation() + " и два числа через пробел");
-            String userMessage = scanner.nextLine();
-            client.send(userMessage);
-
-
-
-
-//        Scanner scanner = new Scanner(System.in); //3. создали сканер
-//        while (true) { //8. На каждой итерации цикла пересоздаем клиентский сокет и клиента
-//            try (Socket socket = new Socket("localhost", 8080)) { //1. созд. обычный сокет, поместили в трай с ресурсами, чтобы потом он закрылся
-//                ExampleClient client = new ExampleClient(socket.getInputStream(), socket.getOutputStream());// 2.создали экземпляр нашего клиента, подаем потоки ввода/вывода - берем их из сокета клиента
-//                System.out.println("Enter message:"); //4. запрашиваем сообщение у пользователя
-//                String userMessage = scanner.nextLine(); //5. считываем сообщение от пользователя
-//                if (userMessage.equals("exit")) { // 7. добавляем breake, если клиент вводит exit
-//                    break;
-//                }
-//                client.send(userMessage);//6. отправляем это сообщение на сервер: вызываем у клиента метод send
-            } catch(IOException e){
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try (Socket socket = new Socket("localhost", 8080)) {
+                ExampleClient client = new ExampleClient(socket.getInputStream(), socket.getOutputStream());
+                System.out.println("Введите операцию из перечисленных: " + client.readOperation() + " и два числа через пробел");
+                String userMessage = scanner.nextLine();
+                if (userMessage.equals("exit")) {
+                    break;
+                }
+                client.send(userMessage);
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-//        }
-
+        }
     }
 }
--- src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw20/ExampleClient.java
+++ src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw20/ExampleClient.java
@@ -3,30 +3,28 @@ package ru.otus.ohmyval.java.basic.homeworks.hw20;
 import java.io.*;
 
 public class ExampleClient implements AutoCloseable {
-    private final DataInputStream inputStream; // 1. инициализировали переменные
+    private final DataInputStream inputStream;
     private final DataOutputStream outputStream;
 
-    public ExampleClient(InputStream inputStream, OutputStream outputStream) { //2.конструктор создали, в него приходят абстрактные потоки
-        this.inputStream = new DataInputStream(inputStream); // 3. создали потоки ввода/вывода
+    public ExampleClient(InputStream inputStream, OutputStream outputStream) {
+        this.inputStream = new DataInputStream(inputStream);
         this.outputStream = new DataOutputStream(outputStream);
     }
+
     public String readOperation() throws IOException {
         return inputStream.readUTF();
-
-
     }
 
-    public void send(String message) throws IOException { // метод будет принимать строчку
-        outputStream.writeUTF(message); // отправляет строку на сервер, ждем ответа
-        outputStream.flush(); // сливаем данные
-        String result = inputStream.readUTF(); // ответ получаем из inputStream, получаем строчку-ответ; здесь ждем ответ от сервера - блокирующая операция
-        System.out.println(result); // печатаем результат от сервера
+    public void send(String message) throws IOException {
+        outputStream.writeUTF(message);
+        outputStream.flush();
+        String result = inputStream.readUTF();
+        System.out.println(result);
     }
 
     @Override
-    public void close() throws Exception { // после завершения работы клиентское приложение закрываем и вызывается метод close
-        inputStream.close(); // закрываем потоки
+    public void close() throws Exception {
+        inputStream.close();
         outputStream.close();
     }
-
 }
