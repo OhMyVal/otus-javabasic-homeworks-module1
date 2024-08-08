f12f51e119dc23f2cd2680d562bf19e28a3f8743
vadorofeeva <makeup-style@mail.ru>
2024-08-08 12:24:40 +0300

    Fixed after PR Everything works

--- src/test/java/ru/otus/ohmyval/java/basic/homeworks/hw30/tests/ArrayAppTests.java
+++ src/test/java/ru/otus/ohmyval/java/basic/homeworks/hw30/tests/ArrayAppTests.java
@@ -1,10 +1,8 @@
 package ru.otus.ohmyval.java.basic.homeworks.hw30.tests;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 
@@ -17,28 +15,13 @@ import static ru.otus.ohmyval.java.basic.homeworks.hw30.ArrayApp.partArray;
 
 public class ArrayAppTests {
 
-    @MethodSource("testArrays")
+    @MethodSource("partArrayReturnsCorrectArrayArgs")
     @ParameterizedTest
-    public void partArrayTest(int[] resArr, int[] enteredArr) {
+    public void partArrayTest_ReturnsCorrectArray(int[] resArr, int[] enteredArr) {
         Assertions.assertArrayEquals(resArr, partArray(enteredArr));
-        Assertions.assertThrows(RuntimeException.class, () -> {
-                        partArray(enteredArr);
-        });
-
-//        Assertions.assertAll(
-//                () -> {
-//                    Assertions.assertThrows(RuntimeException.class, () -> {
-//                        partArray(new int[]{2, 2, 3, 2, 2});
-//                    });
-//                },
-//                () -> {
-//                    Assertions.assertArrayEquals(new int[]{2, 2}, partArray(new int[]{4, 2, 1, 2, 2}));
-//                }
-//        );
-
     }
 
-    public static Stream<Arguments> testArrays() {
+    public static Stream<Arguments> partArrayReturnsCorrectArrayArgs() {
         List<Arguments> out = new ArrayList<>();
         out.add(Arguments.arguments(new int[]{2, 3, 2, 2}, new int[]{2, 3, 2, 2}));
         out.add(Arguments.arguments(new int[]{2, 2}, new int[]{2, 2, 5, 2, 2}));
@@ -46,13 +29,31 @@ public class ArrayAppTests {
         out.add(Arguments.arguments(new int[]{3, 2, 2}, new int[]{1, 1, 3, 2, 2}));
         return out.stream();
     }
-    @MethodSource("initialArrays")
+
+    @MethodSource("partArrayThrowsExceptionWhenNoOneArgs")
     @ParameterizedTest
-    public void checkArrayTest(int[] arr, boolean containsOneAndTwoNumbers){
+    public void partArrayTest_ThrowsExceptionWhenNoOne(int[] enteredArr) {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            partArray(enteredArr);
+        });
+    }
+
+    public static Stream<Arguments> partArrayThrowsExceptionWhenNoOneArgs() {
+        List<Arguments> out = new ArrayList<>();
+        out.add(Arguments.arguments(new int[]{2, 3, 2, 2}));
+        out.add(Arguments.arguments(new int[]{2, 2, 5, 2, 2}));
+        out.add(Arguments.arguments(new int[]{1, 2, 3, 6, 2}));
+        out.add(Arguments.arguments(new int[]{1, 1, 3, 2, 2}));
+        return out.stream();
+    }
+
+    @MethodSource("checkArrayArgs")
+    @ParameterizedTest
+    public void checkArrayTest(int[] arr, boolean containsOneAndTwoNumbers) {
         Assertions.assertEquals(checkArray(arr), containsOneAndTwoNumbers);
     }
 
-    public static Stream<Arguments> initialArrays() {
+    public static Stream<Arguments> checkArrayArgs() {
         List<Arguments> out = new ArrayList<>();
         out.add(Arguments.arguments(new int[]{2, 1, 2, 1, 1}, true));
         out.add(Arguments.arguments(new int[]{1, 1, 2, 1, 1}, true));
